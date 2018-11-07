aeceaaf7f244c3f920733173180f8484882ca147
luis-otavio <luisotavio05@gmail.com>
2018-11-07 11:51:34 -0200

    primeiro commit

---
+++ exercicios/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+import Simples from './components/Simples'
+import ParImpar from './components/ParImpar'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Simples texto='Fexivel!!'/>
+        <ParImpar numero={1} />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  f20: {
+    fontSize: 100,
+  }
+})
